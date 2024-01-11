e55ba79037453dac6510b7f26fac26fb0e4a12c8
Watson, Cory <watsoncl1@ornl.gov>
2024-01-11 11:29:43 -0500

    Add additional ideas to example net config

--- network/service.py
+++ network/service.py
@@ -1,6 +1,7 @@
 class Service:
-    def __init__(self, port, version, name, vulnerabilities=[]):
+    def __init__(self, port, version, name, protocol='tcp', vulnerabilities=[]):
         self.port = port
+        self.protocol = protocol # i.e. tcp or udp
         self.version = version
         self.name = name
         self.vulnerabilities = vulnerabilities

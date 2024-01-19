ac3d4584226aa70061ed7e95679bedf986ba4fec
Watson, Cory <watsoncl1@ornl.gov>
2024-01-19 16:47:52 -0500

    Add firewall/route changes to hosts, subnets, and routers

--- network/router.py
+++ network/router.py
@@ -1,6 +1,70 @@
 class Router:
-    def __init__(self, name, default_route):
+    def __init__(self, name, default_route, routes=[], firewall_rules={}):
+        '''
+        :param str name: name of router
+        :param str default_route: name of router that provides default route
+        :param list[str] routes: additional routes in the routing table
+                (abstracted to router or subnet names)
+        :param dict: firewall_rules: firewall rules dict (emtpy rules = allow all)
+                Example:
+                {'service-name':
+                    {'port': 443,
+                     'proto': 'tcp',
+                     'desc': 'Allow all src to all dest on dest port 443'},
+                 'foo service':
+                    {'port': 3128,
+                     'proto': 'tcp',
+                     'src': ['subnet_a', 'subnet_b'],
+                     'desc': 'Allow only subnet_a and subnet_b to use foo service'
+                     }
+                }
+        '''
         self.name = name
         self.default_route = default_route
-        self.routes = []  # List of routes to other subnets or routers
-        self.firewall_rules = []  # List of firewall rules specific to this router
+        self.routes = routes  # List of routes to other subnets or routers
+        self.firewall_rules = firewall_rules  # dict of firewall rules specific to this router
+
+
+    def get_default_route(self) -> str:
+        return self.default_route
+
+
+    def get_routes(self) -> list[str]:
+        # should the default route be preppended to this list??
+        return self.routes.append(self.default_route)
+
+
+    def add_firewall_rule(self, rule):
+        '''
+        Adds new firewall rule(s)
+
+        :param dict: firewall_rules: firewall rules dict (emtpy rules = allow all)
+                Example:
+                {'service-name':
+                    {'port': 443,
+                     'proto': 'tcp',
+                     'desc': 'Allow all src to all dest on dest port 443'},
+                 'foo service':
+                    {'port': 3128,
+                     'proto': 'tcp',
+                     'src': ['subnet_a', 'subnet_b'],
+                     'desc': 'Allow only subnet_a and subnet_b to use foo service'
+                     }
+                }
+        '''
+        # if a new rule is added that has overlapping keys (i.e. service name)
+        # with an existing rule it will overwrite the previous existing rule
+        self.firewall_rules.update(rule)
+
+
+    def remove_firewall_rule(self, rule_name: str):
+        '''
+        Removes an existing firewall rule
+
+        :param str rule_name: service name of existing fw rule
+        '''
+        try:
+            self.firewall_rules.pop(rule_name)
+        except KeyError as e:
+            # TODO: raise custom exception here?
+            raise e
--- network/subnet.py
+++ network/subnet.py
@@ -5,11 +5,24 @@ class Subnet:
         self.name = name
         self.default_route = default_route
         self.network = ipaddress.IPv4Network(f"{ip_range}", strict=False)
-        self.routes = []  # List of routes to other subnets or routers
-        self.firewall_rules = []  # List of firewall rules specific to this subnet
+        ## not sure if subnets should have routes at this point
+        #self.routes = []  # List of routes to other subnets or routers
+        ## currently I think only routers and hosts should have ACLs
+        #self.firewall_rules = {}  # List of firewall rules specific to this subnet
 
-    def get_network_address(self):
+    def get_network_address(self) -> str:
+        '''
+        Returns the network address of the subnet
+            i.e. '192.168.0.0'
+        '''
         return str(self.network.network_address)
 
-    def get_prefix_length(self):
+    def get_prefix_length(self) -> int:
         return self.network.prefixlen
+
+
+    def get_max_num_hosts(self) -> int:
+        '''
+        Returns number of usable IP address in subnet.
+        '''
+        return self.network.num_addresses - 2

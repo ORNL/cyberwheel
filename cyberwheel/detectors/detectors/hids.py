495a8168862d3ab3a4d9cf7d32f0f2c0de2aadd3
Matthew Dixson (70d) <dixsonmk@ornl.gov>
2024-02-20 12:03:23 -0500

    Fix imports

--- cyberwheel/detectors/detectors/hids.py
+++ cyberwheel/detectors/detectors/hids.py
@@ -2,8 +2,9 @@ import random
 from typing import Iterable
 
 from network.network_base import Network
-from ..detector_base import read_detector_yaml, Detector
-from ..alert import Alert
+from detectors.detectors.probability_detector import ProbabilityDetector
+from detectors.detector_base import read_detector_yaml
+from detectors.alert import Alert
 
 class HIDSDetector(Detector):
     def __init__(self, network: Network) -> None:
--- cyberwheel/detectors/detectors/nids.py
+++ cyberwheel/detectors/detectors/nids.py
@@ -4,7 +4,7 @@ from typing import Iterable
 from network.network_base import Network
 from detectors.detectors.probability_detector import ProbabilityDetector
 from detectors.detector_base import read_detector_yaml
-from ..alert import Alert
+from detectors.alert import Alert
 
 class NIDSDetector(ProbabilityDetector):
     def __init__(self, network: Network) -> None:

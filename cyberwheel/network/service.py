039ff50b367791cbb5cb5a409a4d0d9795961774
Watson, Cory <watsoncl1@ornl.gov>
2024-02-28 10:28:39 -0500

    Use pydantic for Service class

--- cyberwheel/network/service.py
+++ cyberwheel/network/service.py
@@ -1,12 +1,48 @@
-class Service:
-    def __init__(self, name, port, protocol="tcp", version=None, vulnerabilities=[]):
-        self.name = name
-        self.port = port
-        self.protocol = protocol  # i.e. tcp, udp, or icmp
-        self.version = version
-        self.vulnerabilities = vulnerabilities
+#from dataclasses import dataclass, field
+from pydantic import BaseModel, validator, PositiveInt
+from typing import Optional
 
-    def __eq__(self, __value: object) -> bool:
-        if self.name == __value.name:
-            return True
-        return False
+
+class Service(BaseModel):
+    name: str
+    port: PositiveInt
+    protocol: Optional[str] = 'tcp' # default to tcp
+    version: Optional[str] = None
+    vulns: Optional[list] = [] # TODO: list[dict] here? list[Vuln]??
+    description: Optional[str] = ''
+
+    @validator('port')
+    @classmethod
+    def validate_port(cls, port: PositiveInt) -> int:
+        if port not in range(2**16):
+            msg = 'Port should be an integer (1-65535)'
+            raise PortValueError(value=port, message=msg)
+        else:
+            return port
+
+    @validator('protocol')
+    @classmethod
+    def validate_proto(cls, proto) -> str:
+        if proto in ['tcp', 'udp', 'icmp']:
+            return proto
+        else:
+            msg = "Protocol should be 'tcp', 'udp', or 'icmp'"
+            raise ProtocolValueError(value=proto, message=msg)
+
+
+class PortValueError(ValueError):
+    def __init__(self, value: int, message: str) -> None:
+        self.value = value
+        self.message = message
+        super().__init__(message)
+
+
+class ProtocolValueError(ValueError):
+    def __init__(self, value: str, message: str) -> None:
+        self.value = value
+        self.message = message
+        super().__init__(message)
+
+
+    #def __post_init__(self) -> None:
+    #    pass
